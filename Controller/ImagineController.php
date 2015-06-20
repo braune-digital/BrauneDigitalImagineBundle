857de843e38b85cdf5df1f9c59e4be759577eedb
Henrik Braune <hb@braune-digital.com>
2015-06-20 13:05:19 +0200

    Refactoring

---
+++ Controller/ImagineController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace BrauneDigital\ImagineBundle\Controller;
+
+use Imagine\Exception\RuntimeException;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Liip\ImagineBundle\Imagine\Data\DataManager;
+use Liip\ImagineBundle\Imagine\Filter\FilterManager;
+use Liip\ImagineBundle\Exception\Binary\Loader\NotLoadableException;
+use Liip\ImagineBundle\Imagine\Cache\SignerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Liip\ImagineBundle\Controller\ImagineController as BaseController;
+
+class ImagineController extends BaseController
+{
+
+    /**
+     * This action applies a given filter to a given image, optionally saves the image and outputs it to the browser at the same time.
+     *
+     * @param Request $request
+     * @param string $filter
+     * @param string $newName
+     * @param string $path
+     *
+     * @throws \RuntimeException
+     * @throws BadRequestHttpException
+     *
+     * @return RedirectResponse
+     */
+    public function filterWithNewNameAction(Request $request, $filter, $newName, $path = '')
+    {
+
+
+		$newPath = $path;
+		if ($newName != 'null') {
+			$pathInfo = pathinfo($path);
+			$newPath = str_replace($pathInfo['basename'], $newName . '.' . $pathInfo['extension'], $path);
+		}
+
+        try {
+            if (!$this->cacheManager->isStored($newPath, $filter)) {
+				try {
+					$binary = $this->dataManager->find($filter, $path);
+				} catch (NotLoadableException $e) {
+					if ($defaultImageUrl = $this->dataManager->getDefaultImageUrl($filter)) {
+						return new RedirectResponse($defaultImageUrl);
+					}
+
+					throw new NotFoundHttpException('Source image could not be found', $e);
+				}
+
+				$this->cacheManager->store(
+					$this->filterManager->applyFilter($binary, $filter),
+					$newPath,
+					$filter
+				);
+            }
+			return new RedirectResponse($this->cacheManager->resolve($newPath, $filter), 301);
+
+        } catch (RuntimeException $e) {
+            throw new \RuntimeException(sprintf('Unable to create image for path "%s" and filter "%s". Message was "%s"', $path, $filter, $e->getMessage()), 0, $e);
+        }
+    }
+
+    /**
+     * This action applies a given filter to a given image, optionally saves the image and outputs it to the browser at the same time.
+     *
+     * @param Request $request
+     * @param string  $hash
+     * @param string  $path
+     * @param string  $newName
+     * @param string  $filter
+     *
+     * @throws \RuntimeException
+     * @throws BadRequestHttpException
+     *
+     * @return RedirectResponse
+     */
+    public function filterRuntimeWithNewNameAction(Request $request, $hash, $newName, $path, $filter)
+    {
+		$newPath = $path;
+		if ($newName != 'null') {
+			$pathInfo = pathinfo($path);
+			$newPath = str_replace($pathInfo['basename'], $newName . '.' . $pathInfo['extension'], $path);
+		}
+
+        try {
+            $filters = $request->query->get('filters', array());
+
+            if (true !== $this->signer->check($hash, $path, $filters)) {
+                throw new BadRequestHttpException(sprintf(
+                    'Signed url does not pass the sign check for path "%s" and filter "%s" and runtime config %s',
+                    $path,
+                    $filter,
+                    json_encode($filters)
+                ));
+            }
+
+            try {
+                $binary = $this->dataManager->find($filter, $path);
+            } catch (NotLoadableException $e) {
+                if ($defaultImageUrl = $this->dataManager->getDefaultImageUrl($filter)) {
+                    return new RedirectResponse($defaultImageUrl);
+                }
+
+                throw new NotFoundHttpException(sprintf('Source image could not be found for path "%s" and filter "%s"', $path, $filter), $e);
+            }
+
+            $rcPath = $this->cacheManager->getRuntimePath($newPath, $filters);
+
+            $this->cacheManager->store(
+                $this->filterManager->applyFilter($binary, $filter, array(
+                    'filters' => $filters,
+                )),
+                $rcPath,
+                $filter
+            );
+
+            return new RedirectResponse($this->cacheManager->resolve($rcPath, $filter), 301);
+        } catch (RuntimeException $e) {
+            throw new \RuntimeException(sprintf('Unable to create image for path "%s" and filter "%s". Message was "%s"', $hash.'/'.$path, $filter, $e->getMessage()), 0, $e);
+        }
+    }
+}
---
+++ Imagine/Cache/CacheManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace BrauneDigital\ImagineBundle\Imagine\Cache;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager as BaseCacheManager;
+use Liip\ImagineBundle\Binary\BinaryInterface;
+use Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface;
+use Liip\ImagineBundle\Imagine\Filter\FilterConfiguration;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\Event;
+use Liip\ImagineBundle\ImagineEvents;
+use Liip\ImagineBundle\Events\CacheResolveEvent;
+
+class CacheManager extends BaseCacheManager
+{
+
+	protected $newName;
+
+	/**
+	 * @param $path
+	 * @param $filter
+	 * @param array $runtimeConfig
+	 * @param $newName
+	 */
+	public function getBrowserPathWithNewName($path, $filter, array $runtimeConfig = array(), $newName = 'null') {
+		$this->newName = $newName;
+		return $this->getBrowserPath($path, $filter, $runtimeConfig);
+	}
+
+	public function getBrowserPath($path, $filter, array $runtimeConfig = array())
+	{
+
+		$newPath = $path;
+		if ($this->newName != 'null') {
+			$pathInfo = pathinfo($path);
+			$newPath = str_replace($pathInfo['basename'], $this->newName . '.' . $pathInfo['extension'], $path);
+		}
+
+		if (!empty($runtimeConfig)) {
+			$rcPath = $this->getRuntimePath($path, $runtimeConfig);
+			return $this->isStored($rcPath, $filter) ?
+				$this->resolve($rcPath, $filter) :
+				$this->generateUrl($path, $filter, $runtimeConfig, $this->newName)
+				;
+		}
+
+		return $this->isStored($newPath, $filter) ?
+			$this->resolve($newPath, $filter) :
+			$this->generateUrl($path, $filter, array(), $this->newName)
+			;
+	}
+
+
+	public function generateUrl($path, $filter, array $runtimeConfig = array(), $newName = 'null')
+	{
+		$params = array(
+			'path' => ltrim($path, '/'),
+			'filter' => $filter
+		);
+
+		if ($newName != 'null') {
+			$params['newName'] = $newName;
+		}
+
+        if (empty($runtimeConfig)) {
+            $filterUrl = $this->router->generate('liip_imagine_filter', $params, false);
+        } else {
+            $params['filters'] = $runtimeConfig;
+            $params['hash'] = $this->signer->sign($path, $runtimeConfig);
+            $filterUrl = $this->router->generate('liip_imagine_filter_runtime', $params, false);
+        }
+
+        return $filterUrl;
+	}
+
+
+}
