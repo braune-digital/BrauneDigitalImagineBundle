213ee081ebe5c72f8ae76ee465aae4bb1241386e
Henrik Braune <hb@braune-digital.com>
2016-04-05 22:11:15 +0200

    Bugfix

--- Imagine/Cache/CacheManager.php
+++ Imagine/Cache/CacheManager.php
@@ -1,22 +1,32 @@
 <?php
 
 namespace BrauneDigital\ImagineBundle\Imagine\Cache;
-use Liip\ImagineBundle\Exception\Binary\Loader\NotLoadableException;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager as BaseCacheManager;
-use Liip\ImagineBundle\Binary\BinaryInterface;
-use Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface;
-use Liip\ImagineBundle\Imagine\Filter\FilterConfiguration;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\Event;
-use Liip\ImagineBundle\ImagineEvents;
-use Liip\ImagineBundle\Events\CacheResolveEvent;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CacheManager extends BaseCacheManager
+class CacheManager extends BaseCacheManager implements ContainerAwareInterface
 {
 
 	protected $newName;
+
+	protected $resolveInstant = false;
+
+	protected $container;
+
+	protected $dataManager;
+
+	protected $filterManager;
+	/**
+	 * @param ContainerInterface|null $container
+	 */
+	public function setContainer(ContainerInterface $container = null) {
+		$this->resolveInstant = $container->getParameter('braune_digital_imagine.resolve_instant');
+		$this->container = $container;
+
+		$this->dataManager = $this->container->get('liip_imagine.data.manager');
+		$this->filterManager = $this->container->get('liip_imagine.filter.manager');
+	}
 
 	/**
 	 * @param $path
@@ -26,24 +36,24 @@ class CacheManager extends BaseCacheManager
 	 */
 	public function getBrowserPathWithNewName($path, $filter, array $runtimeConfig = array(), $newName = 'null') {
 		$this->newName = $newName;
-		return $this->getBrowserPath($path, $filter, $runtimeConfig);
+		$result = $this->getBrowserPath($path, $filter, $runtimeConfig);
+		//prevent accidental multiple usages
+		$this->newName = null;
+		return $result;
 	}
 
 	public function getBrowserPath($path, $filter, array $runtimeConfig = array())
 	{
-
 		$newPath = $path;
-		if ($this->newName != 'null') {
+		if ($this->newName != null && $this->newName != 'null') {
 			$pathInfo = pathinfo($path);
 			$newPath = str_replace($pathInfo['basename'], $this->newName . '.' . $pathInfo['extension'], $path);
 		}
 
 		if (!empty($runtimeConfig)) {
-			$rcPath = $this->getRuntimePath($path, $runtimeConfig);
-			return $this->isStored($rcPath, $filter) ?
-				$this->resolve($rcPath, $filter) :
-				$this->generateUrl($path, $filter, $runtimeConfig, $this->newName)
-				;
+			$newPath = $this->getRuntimePath($path, $runtimeConfig);
+		} else {
+			$runtimeConfig = array();
 		}
 
 		if($this->isStored($newPath, $filter)) {
@@ -65,12 +75,10 @@ class CacheManager extends BaseCacheManager
 				$path = '';
 			}
 			return $path;
-
 		} else {
 			return $this->generateUrl($path, $filter, $runtimeConfig, $this->newName);
 		}
 	}
-
 
 	public function generateUrl($path, $filter, array $runtimeConfig = array(), $newName = 'null')
 	{
@@ -79,20 +87,20 @@ class CacheManager extends BaseCacheManager
 			'filter' => $filter
 		);
 
-		if ($newName != 'null') {
+		if ($newName != null) {
 			$params['newName'] = $newName;
+		} else {
+			$params['newName'] = 'null';
 		}
 
-        if (empty($runtimeConfig)) {
-            $filterUrl = $this->router->generate('liip_imagine_filter', $params, false);
-        } else {
-            $params['filters'] = $runtimeConfig;
-            $params['hash'] = $this->signer->sign($path, $runtimeConfig);
-            $filterUrl = $this->router->generate('liip_imagine_filter_runtime', $params, false);
-        }
+		if (empty($runtimeConfig)) {
+			$filterUrl = $this->router->generate('liip_imagine_filter', $params, false);
+		} else {
+			$params['filters'] = $runtimeConfig;
+			$params['hash'] = $this->signer->sign($path, $runtimeConfig);
+			$filterUrl = $this->router->generate('liip_imagine_filter_runtime', $params, false);
+		}
 
-        return $filterUrl;
+		return $filterUrl;
 	}
-
-
 }
