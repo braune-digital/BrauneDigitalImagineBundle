b81edbf6ad0e98c933e0e00313db4c4bb5b864ac
Patrick Rathje <pr@braune-digital.com>
2016-04-05 17:54:55 +0200

    Added RuntimeConfigInterface

--- Imagine/Cache/SonataMediaCacheManager.php
+++ Imagine/Cache/SonataMediaCacheManager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace BrauneDigital\ImagineBundle\Imagine\Cache;
+use BrauneDigital\ImagineBundle\Model\RuntimeConfigInterface;
 use Sonata\MediaBundle\Model\Media;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -10,41 +11,86 @@ class SonataMediaCacheManager extends CacheManager implements ContainerAwareInte
 {
 	protected $container;
 
+	/**
+	 * @param ContainerInterface|null $container
+	 */
 	public function setContainer(ContainerInterface $container = null) {
 		$this->container = $container;
 	}
 
+	/**
+	 * @param       $path
+	 * @param       $filter
+	 * @param array $runtimeConfig
+	 * @param null  $newName
+	 *
+	 * @return string
+	 */
 	public function getBrowserPathWithNewName($path, $filter, array $runtimeConfig = array(), $newName = null) {
 
 		if($path instanceof Media) {
 			$media = $path;
 			$path = $this->getPath($media);
 			$newName = $this->getName($media, $newName);
+
+			$runtimeConfig = $this->getRuntimeConfig($media, $runtimeConfig);
 		}
 		return parent::getBrowserPathWithNewName($path, $filter, $runtimeConfig, $newName);
 	}
 
+	/**
+	 * @param string $path
+	 * @param string $filter
+	 * @param array  $runtimeConfig
+	 *
+	 * @return string
+	 */
 	public function getBrowserPath($path, $filter, array $runtimeConfig = array())
 	{
 		if($path instanceof Media) {
 			$media = $path;
 			$path = $this->getPath($media);
+			$runtimeConfig = $this->getRuntimeConfig($media, $runtimeConfig);
 		}
-
 
 		return parent::getBrowserPath($path, $filter, $runtimeConfig);
 	}
 
+	/**
+	 * @param string $path
+	 * @param string $filter
+	 * @param array  $runtimeConfig
+	 * @param string $newName
+	 *
+	 * @return string
+	 */
 	public function generateUrl($path, $filter, array $runtimeConfig = array(), $newName = 'null')
 	{
+		if($path instanceof Media) {
+			$media = $path;
+			$path = $this->getPath($media);
+			$runtimeConfig = $this->getRuntimeConfig($media, $runtimeConfig);
+		}
+
 		return parent::generateUrl($path, $filter, $runtimeConfig, $newName);
 	}
 
+	/**
+	 * @param Media $media
+	 *
+	 * @return mixed
+	 */
 	protected function getPath(Media $media) {
 		$provider = $this->container->get($media->getProviderName());
 		return $provider->generatePublicUrl($media, 'reference');
 	}
 
+	/**
+	 * @param Media $media
+	 * @param null  $newName
+	 *
+	 * @return null|string
+	 */
 	protected function getName(Media $media, $newName = null) {
 
 		if($newName != null) {
@@ -56,5 +102,25 @@ class SonataMediaCacheManager extends CacheManager implements ContainerAwareInte
 				return 'null';
 			}
 		}
+	}
+
+	/**
+	 * @param Media $media
+	 * @param       $runtimeConfig
+	 *
+	 * @return array
+	 * Merge Runtime Configs if the media is implementing the RuntimeConfigInterface
+	 */
+	protected function getRuntimeConfig(Media $media, $runtimeConfig) {
+
+		if($media instanceof RuntimeConfigInterface) {
+
+			$additionalConfig = $media->getRuntimeConfiguration();
+
+			if($additionalConfig && count($additionalConfig)) {
+				return array_replace_recursive($additionalConfig, $runtimeConfig);
+			}
+		}
+		return $runtimeConfig;
 	}
 }
