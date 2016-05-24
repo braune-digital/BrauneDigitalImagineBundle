fa33485c9b8fb7c9cf65426c54e857b3a77adda4
Felix Baltruschat <fb@braune-digital.com>
2016-05-24 19:30:00 +0200

    added greyscale filter

---
+++ Imagine/Filter/Loader/GrayScale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BrauneDigital\ImagineBundle\Imagine\Filter\Loader;
+
+use Imagine\Filter\FilterInterface;
+use Imagine\Gd\Effects;
+use Imagine\Image\ImageInterface;
+use Liip\ImagineBundle\Imagine\Filter\Loader\LoaderInterface;
+
+class Grayscale implements LoaderInterface {
+
+
+
+    /**
+     * @param int $sigma
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ImageInterface $image, array $options = array())
+    {
+
+        $image->effects()->grayscale();
+
+        return $image;
+    }
+}
